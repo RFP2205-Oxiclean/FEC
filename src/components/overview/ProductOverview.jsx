247953ee92ba150223e389ca67b7f650c23ff6b7
Jason Chiou <jaschiou@gmail.com>
2022-07-07 17:15:52 -0700

    renamed folders, added .jsx for each component, rendered to App

---
+++ src/components/overview/ProductOverview.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ProductOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+}
+render() {
+    return (
+        <h1>
+            ProductOverview is working
+        </h1>
+    )
+}
+
+}
+
+export default ProductOverview;
