42ac2bb1ef6a1e28ae7456d998d8c25c85ec6a0a
drumgarage <73360526+drumgarage@users.noreply.github.com>
2022-07-08 15:46:14 -0700

    Work on appjsx (#13)
    * New template including config.js folder / config.example.js folder with API key info.
    
    * Built get request, updateRelatedItems(), getRatingsReviewsData() method in App.jsx.
    
    Co-authored-by: Frederick V Pascua <104282669+McLul@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import {url} from "../config/config.js";
+import {API_KEY} from "../config/config.js";
+import axios from "axios";
 
 import ProductOverview from './components/overview/ProductOverview.jsx'
 import QuestionsAndAnswers from './components/questionsAndAnswers/QuestionsAndAnswers.jsx'
@@ -9,16 +12,65 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            displayedProductId: 40344,
+            relatedItems: [],
+            questionsAndAnswersData: [],
+            ratingsAndReviews: []
 
         };
+
+        this.getRelatedProducts = this.getRelatedProducts.bind(this);
+        this.updateRelatedItemsState = this.updateRelatedItemsState.bind(this);
     }
+
+    debug() {
+        console.log(this.state);
+    }
+
+    // Products GET request to the API
+    getRelatedProducts() {
+        console.log('key', API_KEY);
+        let endPoint = `${url}/products`;
+        console.log('endPoint', endPoint);
+        axios.get(`${url}/products`, {
+            headers: {
+                Authorization: API_KEY
+            }
+        }).then((response) => {
+            this.updateRelatedItemsState(response.data);
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    }
+
+    updateRelatedItemsState(data) {
+        this.setState({
+            relatedItems: data
+        })
+    }
+
+    getRatingsReviewsData() {
+        console.log('getting ratings and reviews');
+        let endPoint = `${url}/products`;
+    }
+
+
+
+
+
+
+
+
+
     render() {
         return (
             <div className = 'app'>
                  <h1>
                 Welcome to React App thats build using Webpack and Babel separately
                 </h1>
-
+                <button onClick = {this.debug.bind(this)}>Debug</button>
+                <button onClick={this.getRelatedProducts}>Get Products</button>
                 <div className = 'product-overview'>
                     <ProductOverview />
                 </div>
