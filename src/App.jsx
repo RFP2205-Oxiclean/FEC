63e39aea2ff7598db26b6373d001e0e320d0f6f0
Shannon Murphy <Murphy1693@gmail.com>
2022-07-08 16:41:13 -0700

    Temp working branch (#14)
    * Commented out some related items functionality, changed folder names
    
    * Working branch to merge, removed all state from App.jsx;

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,46 @@
 import React from "react";
-import {url} from "../config/config.js";
-import {API_KEY} from "../config/config.js";
+import { url } from "../config/config.js";
+import { API_KEY } from "../config/config.js";
 import axios from "axios";
 
 import ProductOverview from './components/overview/ProductOverview.jsx'
 import QuestionsAndAnswers from './components/questionsAndAnswers/QuestionsAndAnswers.jsx'
 import RatingsReviews from './components/ratingsReviews/RatingsReviews.jsx'
-import RelatedItems from './components/relatedItems/RelatedItems.jsx'
+// import RelatedItems from './components/relatedItems/RelatedItems.jsx'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            displayedProductId: 40344,
-            relatedItems: [],
-            questionsAndAnswersData: [],
-            ratingsAndReviews: []
-
+            displayedProductId: 41314,
+            // relatedItems: [],
+            // questionsAndAnswersData: [],
+            // ratingsAndReviews: []
         };
-
-        this.getRelatedProducts = this.getRelatedProducts.bind(this);
-        this.updateRelatedItemsState = this.updateRelatedItemsState.bind(this);
     }
-
-    debug() {
-        console.log(this.state);
-    }
-
-    // Products GET request to the API
-    getRelatedProducts() {
-        console.log('key', API_KEY);
-        let endPoint = `${url}/products`;
-        console.log('endPoint', endPoint);
-        axios.get(`${url}/products`, {
-            headers: {
-                Authorization: API_KEY
-            }
-        }).then((response) => {
-            this.updateRelatedItemsState(response.data);
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-    }
-
-    updateRelatedItemsState(data) {
-        this.setState({
-            relatedItems: data
-        })
-    }
-
-    getRatingsReviewsData() {
-        console.log('getting ratings and reviews');
-        let endPoint = `${url}/products`;
-    }
-
-
-
-
-
-
-
-
 
     render() {
         return (
-            <div className = 'app'>
-                 <h1>
-                Welcome to React App thats build using Webpack and Babel separately
+            <div className='app'>
+                <h1>
+                    Welcome to React App thats build using Webpack and Babel separately
                 </h1>
-                <button onClick = {this.debug.bind(this)}>Debug</button>
-                <button onClick={this.getRelatedProducts}>Get Products</button>
-                <div className = 'product-overview'>
-                    <ProductOverview />
+                <div className='product-overview'>
+                    <ProductOverview
+                        product_id={this.state.displayedProductId}
+                    />
                 </div>
 
-                <div className = 'related-items'>
-                    <RelatedItems />
+                <div className='questions-and-answers'>
+                    <QuestionsAndAnswers
+                        product_id={this.state.displayedProductId}
+                    />
                 </div>
 
-                <div className = 'questions-and-answers'>
-                    <QuestionsAndAnswers />
-                </div>
-
-                <div className = 'ratings-reviews'>
-                    <RatingsReviews />
+                <div className='ratings-reviews'>
+                    <RatingsReviews
+                        product_id={this.state.displayedProductId}
+                    />
                 </div>
             </div>
 
---
+++ src/components/ratingsReviews/RatingsReviews.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import axios from 'axios';
+
+class RatingsReviews extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    getReviewList(id) {
+    let endPoint = `${url}/reviews/`;
+    axios.get(endPoint, {
+      params: {
+        product_id: this.state.displayedProductId
+      },
+      headers: {
+        Authorization: API_KEY
+      }
+    })
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((err) => {
+      console.error('Error in getRatingsReviewsData response: ', err);
+    })
+    }
+
+    getRatingsList(id) {
+    let endPoint = `${url}/reviews/meta`;
+    axios.get(endPoint, {
+        params: {
+          product_id: this.state.displayedProductId
+      },
+        headers: {
+          Authorization: API_KEY
+      }
+    })
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((err) => {
+      console.error('Error in getRatingsReviewsData response: ', err);
+    })
+    }
+
+
+    render() {
+        return (
+            <h1>
+                Ratings and Reviews is working
+            </h1>
+        )
+    }
+
+}
+
+
+export default RatingsReviews;
