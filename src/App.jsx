1e9505c7a896cc474d3aa075de8cac6c8344423f
Shannon Murphy <Murphy1693@gmail.com>
2022-07-08 15:52:51 -0700

    Commented out some related items functionality, changed folder names

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {url} from "../config/config.js";
-import {API_KEY} from "../config/config.js";
+import { url } from "../config/config.js";
+import { API_KEY } from "../config/config.js";
 import axios from "axios";
 
 import ProductOverview from './components/overview/ProductOverview.jsx'
 import QuestionsAndAnswers from './components/questionsAndAnswers/QuestionsAndAnswers.jsx'
 import RatingsReviews from './components/ratingsReviews/RatingsReviews.jsx'
-import RelatedItems from './components/relatedItems/RelatedItems.jsx'
+// import RelatedItems from './components/relatedItems/RelatedItems.jsx'
 
 class App extends React.Component {
     constructor(props) {
@@ -18,42 +18,24 @@ class App extends React.Component {
             ratingsAndReviews: []
 
         };
-
-        this.getRelatedProducts = this.getRelatedProducts.bind(this);
-        this.updateRelatedItemsState = this.updateRelatedItemsState.bind(this);
     }
 
-    debug() {
+    myDebugger() {
         console.log(this.state);
-    }
-
-    // Products GET request to the API
-    getRelatedProducts() {
-        console.log('key', API_KEY);
-        let endPoint = `${url}/products`;
-        console.log('endPoint', endPoint);
-        axios.get(`${url}/products`, {
-            headers: {
-                Authorization: API_KEY
-            }
-        }).then((response) => {
-            this.updateRelatedItemsState(response.data);
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-    }
-
-    updateRelatedItemsState(data) {
-        this.setState({
-            relatedItems: data
-        })
     }
 
     getRatingsReviewsData() {
         console.log('getting ratings and reviews');
-        let endPoint = `${url}/products`;
+        let endPoint = `${url}/reviews`;
+        axios({
+            method: 'GET',
+            headers: {
+                Authorization: API_KEY
+            }
+        })
+
     }
+
 
 
 
@@ -65,25 +47,25 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className = 'app'>
-                 <h1>
-                Welcome to React App thats build using Webpack and Babel separately
+            <div className='app'>
+                <h1>
+                    Welcome to React App thats build using Webpack and Babel separately
                 </h1>
-                <button onClick = {this.debug.bind(this)}>Debug</button>
-                <button onClick={this.getRelatedProducts}>Get Products</button>
-                <div className = 'product-overview'>
+                <button onClick={this.myDebugger.bind(this)}>Debug</button>
+                <button onClick={this.getRatingsReviewsData}>Get Ratings/Reviews</button>
+                <div className='product-overview'>
                     <ProductOverview />
                 </div>
-
+                {/*
                 <div className = 'related-items'>
                     <RelatedItems />
-                </div>
+                </div> */}
 
-                <div className = 'questions-and-answers'>
+                <div className='questions-and-answers'>
                     <QuestionsAndAnswers />
                 </div>
 
-                <div className = 'ratings-reviews'>
+                <div className='ratings-reviews'>
                     <RatingsReviews />
                 </div>
             </div>
