c2cb0dd09a66fbac8bb17096ec2863bec6f3cd6b
Frederick V Pascua <frederickvpascua@gmail.com>
2022-07-14 19:32:02 -0700

    fixed name of answerer in userInfo.jsx

--- src/components/questionsAndAnswers/questionList/answerList/answer/userInfo/UserInfo.jsx
+++ src/components/questionsAndAnswers/questionList/answerList/answer/userInfo/UserInfo.jsx
@@ -3,12 +3,8 @@ import {format} from 'date-fns';
 
 
 const UserInfo = (props) => {
-    let increaseMonthLegibility = String(Number.parseInt(props.data.date.substr(5,2))-1)
-    console.log(increaseMonthLegibility)
-    let theDate = new Date( props.data.date.substr(0,4), increaseMonthLegibility , props.data.date.substr(8,2) )
-
-  console.log(props.data.date.substr(0,10))
-    let user = <p className="user-info">by {props.data.user} - {format(theDate ,'MMMM d, Y')}</p>
+    let theDate = new Date( props.data.date.substr(0,4), props.data.date.substr(5,2), props.data.date.substr(8,2) )
+    let user = <p className="user-info">by {props.data.answerer_name} - {format(theDate ,'MMMM d, Y')}</p>
     let yes  = <p className="helpful-event" >Yes ({props.data.helpfulness})</p>
     let report = <p className="report-event" > Report </p>
 
--- src/components/questionsAndAnswers/questionList/question/infoTab/InfoTab.jsx
+++ src/components/questionsAndAnswers/questionList/question/infoTab/InfoTab.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const InfoTab = (props) => {
-    let yes = <p className="helpful-event" >Yes ({props.question.question_helpfulness})</p>;
+    let yes = <p className="helpful-event"  >Yes ({props.question.question_helpfulness})</p>;
     let add = <p className="add-answer-vent" >Add Answer</p>;
     return <div className="info-tab"> Helpful? {yes} | {add} </div>
 }
