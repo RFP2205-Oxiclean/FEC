2a28e4ede07dd0d24df45a702a58b0de8fa428e7
Frederick V Pascua <frederickvpascua@gmail.com>
2022-07-16 19:17:14 -0700

    implemented a dyamic buffer images so reload is not necessary

--- src/components/questionsAndAnswers/questionList/answerList/answer/photoList/photo/Photo.jsx
+++ src/components/questionsAndAnswers/questionList/answerList/answer/photoList/photo/Photo.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+const Buffer = require('buffer').Buffer
 
 class Photo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            photo: null
         }
+    }
+
+    componentDidMount() {
+        this.getImage();
+    }
+
+    componentDidUpdate() {
+    }
+
+    getImage() {
+      if (this.props.url !== undefined) {
+          let endPoint = this.props.url;
+          axios.get(endPoint, {
+              responseType:'arraybuffer'
+          })
+          .then((res)=>{
+              this.state.photo = Buffer.from(res.data,'binary').toString('base64');
+              this.setState(JSON.parse(JSON.stringify(this.state)))
+          })
+          .catch((err)=>console.error(err))
+          }
+
     }
 
     toggleModal () {
@@ -15,9 +39,8 @@ class Photo extends React.Component {
     render() {
         return (
             <div className="photo-container">
-                <img src={this.props.url} className="photo">
 
-                </img>
+                <img src={'data:image/jpeg;base64, '+this.state.photo} className="photo" />
             </div>)
       }
 
